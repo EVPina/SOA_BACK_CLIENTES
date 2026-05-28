1c8dd2e155c2e08d64deb6a4b971c7b33e26bf64
enzo <enzo.vp@outlook.com>
2026-05-27 21:04:46 -0500

    git initial project

---
+++ src/main/java/com/soa/soaclientes/SoaclientesApplication.java
@@ -0,0 +1,13 @@
+package com.soa.soaclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaclientesApplication.class, args);
+	}
+
+}
